00100e715d953d07239feea8339b164bebcbb9de
NIkita <mykytabaranov@gmail.com>
2025-10-06 19:00:26 +0300

    refactoring

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -4,7 +4,15 @@ import { defineNuxtConfig } from 'nuxt/config'
 export default defineNuxtConfig({
   app: {
     head: {
+      meta: [
+        { name: 'apple-mobile-web-app-title', content: 'Notes' }
+      ],
       link: [
+        { rel: 'shortcut icon', type: 'image/x-icon', href: 'favicon.ico' },
+        { rel: 'apple-touch-icon', sizes: '180x180', href: 'apple-touch-icon.png' },
+        { rel: 'icon', type: 'image/png', sizes: '96x96', href: 'favicon-96x96.png' },
+        { rel: 'icon', type: 'image/svg+xml', href: 'favicon.svg' },
+        { rel: 'manifest', href: 'site.webmanifest' },
         {
           rel: 'preconnect',
           href: 'https://fonts.googleapis.com"',
